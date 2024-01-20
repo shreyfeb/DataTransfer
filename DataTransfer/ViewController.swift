41c802ddb894694a54ccb2921a726b1907f136c6
shreyfeb <shreym1008@gmail.com>
2024-01-19 23:54:38 -0700

    api call

--- DataTransfer/ViewController.swift
+++ DataTransfer/ViewController.swift
@@ -6,17 +6,20 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ViewController1: UIViewController {
+    
+    let headers = [
+        "X-RapidAPI-Key": "72e817cad9msh0fab48ceb9d1e52p1ead45jsn96d6958341c6",
+        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
+    ]
+    
+    let url = URL(string: "https://api.example.com/data")!
 
-    @IBOutlet weak var textField: UITextField!
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"  // Or "POST", "PUT", etc.
+    request.allHTTPHeaderFields = headers
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showViewController2" {
-            let destinationVC = segue.destination as! ViewController2
-            destinationVC.receivedData = textField.text ?? ""
-        }
-    }
 }
-
-
